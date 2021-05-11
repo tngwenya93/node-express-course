43d3bee0c8b275cc75b1d3c1f16c937421ee5d5b
tngwenya93 <tngwenya.315@gmail.com>
2021-05-11 13:07:06 +0000

    add first GET route

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ port= process.env.PORT
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+
+
+app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              user: mockUserData
+         })
+    })
