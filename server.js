ec07fe442f5ab9798cb4e51f3e11ac78fa62a43f
tngwenya93 <tngwenya.315@gmail.com>
2021-05-11 13:15:36 +0000

    add login POST route

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 require('dotenv').config();
 port= process.env.PORT
 
-app.listen(port, () => {
-    console.log(`Server started on ${port}`);
-});
+app.use(json());
+
+
+
 
 const mockUserData=[
     {name:'Mark'},
@@ -29,4 +30,29 @@ app.get('/users/:id', (req, res) => {
         user: req.params.id
     });
 
+});
+
+app.post('/login', (req, res) => {
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
 });
